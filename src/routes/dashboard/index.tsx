9d5627ab44efa430a496602a2f59b47889d57f15
wobsoriano <sorianorobertc@gmail.com>
2024-06-15 11:25:57 -0700

    protect dashboard page

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,6 +1,18 @@
 import { Title } from "@solidjs/meta";
+import { createEffect, createMemo } from "solid-js";
+import { useClerk } from "~/components/ClerkProvider";
 
 export default function Dashboard() {
+  const { clerk, loaded } = useClerk()
+
+  const hasActiveSessions = createMemo(() => loaded() && clerk()?.client?.activeSessions?.length > 0)
+
+  createEffect(() => {
+    if (!hasActiveSessions()) {
+      void clerk()?.redirectToAfterSignOut()
+    }
+  })
+
   return (
     <main>
       <Title>Dashboard</Title>
