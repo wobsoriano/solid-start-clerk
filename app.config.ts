b521c6f48753d36e0d16e13c1cf8c31ac1f9c62d
wobsoriano <sorianorobertc@gmail.com>
2024-06-15 13:30:22 -0700

    set preset

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  middleware: "./src/middleware.ts"
+  middleware: "./src/middleware.ts",
+  server: {
+    preset: 'vercel'
+  }
 });
